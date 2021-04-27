e75e0c651644cef2da1584624124aa3dc150fd35
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-04-27 13:37:42 +0530

    add_users_resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "deep", password: "123456")
+User.create(username: "vivek", password: "123456")
+User.create(username: "kapil", password: "123456")
+User.create(username: "raghu", password: "123456")
+User.create(username: "ravi", password: "123456")
